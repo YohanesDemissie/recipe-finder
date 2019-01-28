572e1a1ad51ebb568d8fee94029ab6f1fd4787b6
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-01-27 17:17:14 -0800

    added favorites action/reducer to allow user to star favorited recipes. now creating the component to render users favorite recipes

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { favoriteRecipe } from '../actions';
 
 class RecipeItem extends Component {
+  constructor() {
+    super();
+
+    this.state = { //setting up a boolean for true or false of favorite clicked. setting default as false
+      favorited: false,
+    }
+  }
+
+  favorite(recipe) {
+    this.props.favoriteRecipe(recipe); //switch boolean state to 'true' on click rendered on button tag below
+    this.setState({favorited: true});
+  }
+
   render() {
     let { recipe } = this.props
     return(
       <div className="recipe-item">
+      {
+        this.state.favorited //if the state of favorited becomes true (ie when the star button is clicked)...
+          ?
+            <div
+              className="star">&#9733;</div> // ...the star icon is filled through the unicode of a colored store '&#9733;'
+            : //...otherwise, the star button is transparent by default (false) with the given unicode '&#9734;'
+            <div
+              className="star"
+              onClick={() => this.favorite(recipe)}
+            >
+              &#9734;
+            </div>
+      }
         <div className="recipe-text">
           <a href={recipe.href}>
             <h4>{recipe.title}</h4>
           </a>
           <p>{recipe.ingredients}</p>
         </div>
-          <img 
-            src={recipe.thumbnail} 
+          <img
+            src={recipe.thumbnail}
             alt={recipe.title}
             className="recipe-img"
           />
@@ -21,4 +49,4 @@ class RecipeItem extends Component {
   }
 }
 
-export default RecipeItem
+export default connect(null, { favoriteRecipe})(RecipeItem)
